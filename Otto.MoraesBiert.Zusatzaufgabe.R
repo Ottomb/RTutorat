e739e84115ec386c8b6e1960bab73e6590d0e9b6
Ottomb <otto.moraes.biert@gmail.com>
2020-06-09 16:19:30 +0200

    Add files via upload

--- Otto.MoraesBiert.Zusatzaufgabe.R
+++ Otto.MoraesBiert.Zusatzaufgabe.R
@@ -13,30 +13,35 @@ summarise(StudPerf)
 #b
 ggplot(data = StudPerf, aes(x = "", y = math_score)) +
   geom_boxplot() +
-  labs(x= "Schüler*innen", y= "Mathe Punktzahlen") +
-  theme_economist() + 
-  scale_color_economist()
+  labs(x = "Schüler*innen", y = "Mathe Punktzahlen", title = "Titel")
 
 #c
-Mittelwert <- (mean(StudPerf$math_score) + mean(StudPerf$reading_score) + mean(StudPerf$writing_score)) / 3
-Median <- data.frame(Werte = c(StudPerf[,"math_score"], StudPerf[,"writing_score"], StudPerf[,"reading_score"]))
-Medianfin <- median(Median$Werte)
+AlleScores <- data.frame(Werte = c(StudPerf[,"math_score"], StudPerf[,"writing_score"], StudPerf[,"reading_score"]))
+Mittelwert <- mean(AlleScores$Werte)
+Median <- median(AlleScores$Werte)
 
 #2
+#a
+lineareRegression <- lm(formula = reading_score ~ writing_score, data = StudPerf)
+summary(lineareRegression)
+
+#b
 ggplot(data = StudPerf, aes(x = reading_score, y = writing_score)) +
   geom_point() +
-  geom_smooth(method = "lm", se = FALSE)
+  geom_smooth(method = "lm", se = FALSE) +
+  labs(x = "Lese score", y = "Schreib score", title= "Lineare Regression", subtitle = "Zusammenhang der Lese- und Schreibfähigkeiten")
 
 #c
-lineareRegression <- lm(formula = reading_score ~ writing_score, data = StudPerf)
 lineareRegression[["coefficients"]][["writing_score"]] * 75 + lineareRegression[["coefficients"]][["(Intercept)"]]
 
 #3
 #a
 Prep <- StudPerf %>% 
   filter(test_preparation_course == "completed") %>% 
-  sample_n(300)
+  sample_n(300) %>% 
+  select(math_score)
 
 NoPrep <- StudPerf %>% 
   filter(test_preparation_course == "none") %>% 
-  sample_n(300)
+  sample_n(300) %>% 
+  select(math_score)

0bf8aca46d47e2ad7dee055492aca752a8018837
Ottomb <otto.moraes.biert@gmail.com>
2020-06-16 17:24:50 +0200

    Add files via upload

--- Otto.MoraesBiert.Zusatzaufgabe.R
+++ Otto.MoraesBiert.Zusatzaufgabe.R
@@ -13,7 +13,9 @@ summarise(StudPerf)
 #b
 ggplot(data = StudPerf, aes(x = "", y = math_score)) +
   geom_boxplot() +
-  labs(x = "Schüler*innen", y = "Mathe Punktzahlen", title = "Titel")
+  scale_y_continuous(breaks = c(0, 20, 40, 60, 80, 100)) +
+  labs(x = "Schüler*innen", y = "Mathe Punktzahlen") +
+  theme_economist() + scale_colour_economist()
 
 #c
 AlleScores <- data.frame(Werte = c(StudPerf[,"math_score"], StudPerf[,"writing_score"], StudPerf[,"reading_score"]))
@@ -22,20 +24,24 @@ Median <- median(AlleScores$Werte)
 
 #2
 #a
-lineareRegression <- lm(formula = reading_score ~ writing_score, data = StudPerf)
+lineareRegression <- lm(formula = writing_score ~ reading_score, data = StudPerf)
 summary(lineareRegression)
 
 #b
 ggplot(data = StudPerf, aes(x = reading_score, y = writing_score)) +
   geom_point() +
   geom_smooth(method = "lm", se = FALSE) +
-  labs(x = "Lese score", y = "Schreib score", title= "Lineare Regression", subtitle = "Zusammenhang der Lese- und Schreibfähigkeiten")
+  labs(x = "Lese score", y = "Schreib score", title= "Lineare Regression", subtitle = "Zusammenhang der Lese- und Schreibfähigkeiten")+
+  scale_y_continuous(breaks = c(0, 20, 40, 60, 80, 100)) +
+  scale_x_continuous(breaks = c(0, 20, 40, 60, 80, 100)) +
+  theme_economist()+
+  scale_colour_economist()
 
 #c
 lineareRegression[["coefficients"]][["writing_score"]] * 75 + lineareRegression[["coefficients"]][["(Intercept)"]]
 
 #d
-lineareRegressionGender <- lm(formula = reading_score ~ writing_score + gender, data = StudPerf)
+lineareRegressionGender <- lm(formula = writing_score ~ reading_score + gender, data = StudPerf)
 summary(lineareRegressionGender)
 
 #3
